6cfcbf77b919b39fbd2599c1695ce91acfce8bf5
hit0tsu__ <Maltzev.alexandr@gmail.com>
2022-04-24 03:51:05 +0300

    настроил билд проекта в .exe файл

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -4,7 +4,6 @@ import { Container, Image, Text } from './styles'
 export function Greetings() {
   function handleSayHello() {
     window.Main.sendMessage('Hello World');
-
     console.log('Message sent! Check main process log in terminal.')
   }
 
